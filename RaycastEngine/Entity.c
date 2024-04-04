9cb22062e6008b869b1730b87fccef0df3cd288e
Stephen Hyberger <stephen.hyberger@novaengine.net>
2024-04-04 00:08:18 -0500

    Removed all inline functions and replaced them with regular functions.

--- RaycastEngine/Entity.c
+++ RaycastEngine/Entity.c
@@ -77,3 +77,139 @@ struct sEntity_Data* EntityDeepCopy(struct sEntity_Data* original)
 	}
 	return ret_val;
 }
+
+//Runtime functions
+void DoInput(struct sPlayer_Controller* cntrl, SDL_Event* event)
+{
+	if(cntrl->IsPlayer)
+	{
+		//Get key state if down.
+		if(event->type == SDL_KEYDOWN)
+		{
+			if(event->key.keysym.sym == SDLK_w)
+			{
+				cntrl->Keys |= W_KEY;
+				if(cntrl->Keys & S_KEY) 
+					cntrl->Keys &= ~S_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_d)
+			{
+				cntrl->Keys |= D_KEY;
+				if(cntrl->Keys & A_KEY) 
+					cntrl->Keys &= ~A_KEY;				
+			}
+			else if(event->key.keysym.sym == SDLK_s)
+			{
+				cntrl->Keys |= S_KEY;
+				if(cntrl->Keys & W_KEY) 
+					cntrl->Keys &= ~W_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_a)
+			{
+				cntrl->Keys |= A_KEY;
+				if(cntrl->Keys & D_KEY) 
+					cntrl->Keys &= ~D_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_q)
+			{
+				cntrl->Keys |= Q_KEY;
+				if(cntrl->Keys & E_KEY) 
+					cntrl->Keys &= ~E_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_e)
+			{
+				cntrl->Keys |= E_KEY;
+				if(cntrl->Keys & D_KEY) 
+					cntrl->Keys &= ~D_KEY;
+			}
+		}
+		//Get key state if up.
+		else if(event->type == SDL_KEYUP)
+		{
+			if(event->key.keysym.sym == SDLK_w)
+			{
+				cntrl->Keys &= ~W_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_d)
+			{
+				cntrl->Keys &= ~D_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_s)
+			{
+				cntrl->Keys &= ~S_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_a)
+			{
+				cntrl->Keys &= ~A_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_q)
+			{
+				cntrl->Keys &= ~Q_KEY;
+			}
+			else if(event->key.keysym.sym == SDLK_e)
+			{
+				cntrl->Keys &= ~E_KEY;
+			}
+		}
+	}
+}
+
+void DoMove(struct sEntity_Data* ent, double delta)
+{
+	static struct Float2D Direction;
+
+	if(ent->Controller.Keys & Q_KEY)
+	{
+		ent->Look -= PLAYER_TURN_RATE_PER_SEC * delta;
+		if(ent->Look >= FULL_CIRCLE)
+			ent->Look -= FULL_CIRCLE;
+		else if(ent->Look < 0)
+			ent->Look += FULL_CIRCLE;
+	}
+	if(ent->Controller.Keys & E_KEY)
+	{
+		ent->Look += PLAYER_TURN_RATE_PER_SEC * delta;
+		if(ent->Look >= FULL_CIRCLE)
+			ent->Look -= FULL_CIRCLE;
+		else if(ent->Look < 0)
+			ent->Look += FULL_CIRCLE;
+	}
+
+	Direction.X = cos(ent->Look);
+	Direction.Y = sin(ent->Look);
+
+	//Calculate velocity from key state.
+	if(ent->Controller.Keys & W_KEY)
+	{
+		ent->Velocity.X += Direction.X;
+		ent->Velocity.Y += Direction.Y;
+	}
+	if(ent->Controller.Keys & S_KEY)
+	{
+		ent->Velocity.X += -Direction.X;
+		ent->Velocity.Y += -Direction.Y;
+	}
+	if(ent->Controller.Keys & D_KEY)
+	{
+		ent->Velocity.X += -Direction.Y;
+		ent->Velocity.Y += Direction.X;
+	}
+	if(ent->Controller.Keys & A_KEY)
+	{
+		ent->Velocity.X += Direction.Y;
+		ent->Velocity.Y += -Direction.X;
+	}
+
+	//Normalize and apply.
+	if(ent->Velocity.X != 0 || ent->Velocity.Y != 0) 
+	{	
+		double magnitude = sqrt((ent->Velocity.X) * (ent->Velocity.X) + (ent->Velocity.Y) * (ent->Velocity.Y));
+		ent->Velocity.X /= magnitude;
+		ent->Velocity.Y /= magnitude;
+		ent->Velocity.X *= PLAYER_FEET_PER_SEC * delta;
+		ent->Velocity.Y *= PLAYER_FEET_PER_SEC * delta;
+
+		ent->Location = fAdd(ent->Location, ent->Velocity);
+		ent->Velocity.X = ent->Velocity.Y = 0;
+	}
+}
--- RaycastEngine/Viewport.c
+++ RaycastEngine/Viewport.c
@@ -47,3 +47,66 @@ void DrawFirstPersonView(struct SDL_Renderer* renderer, struct sWorld_Data** wrl
 		}
 	}
 }
+
+double CastRay(double angle, struct sEntity_Data* viewer, struct sLevel_Data* lvl, struct Float2D* ray_hit, char* x_or_y)
+{
+	struct Float2D hit_x = {0}, hit_y = {0}, offset = {0};
+	struct Float2D source = viewer->Location;
+	double distX=1024, distY=1024, tangent = tan(angle), cotangent = 1/tangent, correction_angle = 0;
+	int count = 0, index;
+
+	if(angle>=FULL_CIRCLE) angle-=FULL_CIRCLE; if(angle<0) angle+=FULL_CIRCLE;
+
+	if(angle<QUARTER_CIRCLE || angle>THREE_QUARTER_CIRCLE) { hit_x.X = (int)source.X + 1; hit_x.Y = tangent * (hit_x.X - source.X) + source.Y; offset.X = 1; offset.Y = offset.X * tangent; }
+	else if(angle>QUARTER_CIRCLE && angle<THREE_QUARTER_CIRCLE) { hit_x.X = (int)source.X; hit_x.Y = tangent * (hit_x.X - source.X) + source.Y; offset.X = -1; offset.Y = offset.X * tangent; }
+
+	while(count < VIEW_MAX)
+	{
+		index = (int)hit_x.Y * lvl->Size.X + (int)hit_x.X;
+		if
+		(
+			hit_x.Y < lvl->Size.Y && 
+			hit_x.Y >= 0 && 
+			(lvl->TileData[index].Type || lvl->TileData[index - 1].Type)
+		)
+		{
+			distX = sqrt((source.X-hit_x.X)*(source.X-hit_x.X) + (source.Y-hit_x.Y)*(source.Y-hit_x.Y));
+			break;
+		}
+		else { hit_x.X += offset.X; hit_x.Y += offset.Y; ++count; }
+	}
+
+	count = 0;
+	if(angle<M_PI) { hit_y.Y = (int)source.Y + 1; hit_y.X = cotangent * (hit_y.Y - source.Y) + source.X; offset.Y = 1; offset.X = offset.Y * cotangent; }
+	else if(angle>M_PI) { hit_y.Y = (int)source.Y; hit_y.X = cotangent * (hit_y.Y - source.Y) + source.X; offset.Y = -1; offset.X = offset.Y * cotangent; }
+
+	while(count < VIEW_MAX)
+	{
+		index = (int)hit_y.Y * lvl->Size.X + (int)hit_y.X;
+		if
+		(	
+			hit_y.X < lvl->Size.X && 
+			hit_y.X >= 0 && 
+			(lvl->TileData[index].Type || lvl->TileData[index - lvl->Size.X].Type)
+		)
+		{
+			distY = sqrt((source.X-hit_y.X)*(source.X-hit_y.X) + (source.Y-hit_y.Y)*(source.Y-hit_y.Y));
+			break;		
+		}
+		else { hit_y.X += offset.X; hit_y.Y += offset.Y; ++count; }
+	}
+
+	correction_angle = viewer->Look - angle; if(correction_angle<0) correction_angle+=FULL_CIRCLE; if(correction_angle>=FULL_CIRCLE) correction_angle-=FULL_CIRCLE;
+	if(distX < distY)
+	{
+		if(x_or_y != NULL) *x_or_y = 1;
+		if(ray_hit != NULL) { ray_hit->X = hit_x.X; ray_hit->Y = hit_x.Y; }
+		return distX * cos(correction_angle);
+	}
+	else
+	{
+		if(x_or_y != NULL) *x_or_y = 0;
+		if(ray_hit != NULL) { ray_hit->X = hit_y.X; ray_hit->Y = hit_y.Y; }
+		return distY * cos(correction_angle);
+	}
+}
